930d0894df7cdc07e5f8d29b6c51ee03b7d1b22e
Brice Lin <bricejlin@gmail.com>
2015-10-29 15:17:51 +0100

    Update data schema for better parsing using Chart.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('superagent');
 const Promise = require('bluebird');
 const Firebase = require('firebase');
+const curry = require('curry');
 
 const config = require('./conf.json');
 const AUTH = config.stubHubAuth;
@@ -9,7 +10,7 @@ const EVENTS_URI = 'search/catalog/events/v2?id=';
 
 const stubHubFactory = require('./stubhubClient');
 
-const rootRef = new Firebase('https://stubhub-tracker.firebaseio.com/events');
+const rootRef = new Firebase('https://stubhub-tracker.firebaseio.com/');
 
 const ONE_MINUTE = 1000 * 60;
 const ONE_HOUR = ONE_MINUTE * 60;
@@ -26,43 +27,64 @@ const url = (id) => stubHubFactory.STUBHUB_BASE_URL + EVENTS_URI + id;
 
 const stubHub = stubHubFactory.create(AUTH);
 
+const updateTicketInfo = curry(updateTicketInfoInFirebase);
+
 // setInterval(fetch, ONE_HOUR * 2);
 fetch();
 
-function fetch () {
+function fetch () {  
+  const date = (new Date()).toISOString().split('.')[0];
+
   sHEventCodes.forEach(function (event) {
     stubHub.fetch(url(event))
       .then(parseEventRes)
-      .then(sendToFirebase);
+      .then(addEventToFirebase)
+      .then(updateTicketInfo(date));
   });
 }
 
-function sendToFirebase (event) {
+/**
+ * Update ticket info in firebase
+ * @param {string} date - iso datetime string
+ * @param {obj} event
+ * @return {void}
+ */
+function updateTicketInfoInFirebase (date, event) {
+  var info = {
+    eventId: event.id,
+    ticketInfo: event.ticketInfo
+  };
+
+  rootRef.child('ticketInfo/' + date).push().set(info, function (err) {
+    if (err) {
+      console.log(';(');
+    } else {
+      console.log('nice!');
+    }
+  });
+};
+
+function addEventToFirebase (event) {
   if (event === null) return;
+  var deferred = Promise.defer();
   
   // Check listing already exists
-  rootRef.child(event.id).once('value', function (snap) {
-    // If it exists, append updated ticket prices
-    if (snap.val()) {
-      const ticketInfo = addDate(event.ticketInfo);
-      rootRef.child(event.id + '/tickets').push().set(ticketInfo, function (err) {
-        if (err) {
-          console.log('womp');
-        } else {
-          console.log('ticket prices updated!');
-        }
-      });
-    } else {
-      // Otherwise create a new listing
-      rootRef.child(event.id).set(event, function (err) {
+  rootRef.child('events/' + event.id).once('value', function (snap) {
+    // If not, add event to firebase
+    if (!snap.val()) {
+      rootRef.child('events/' + event.id).set(event, function (err) {
         if (err) {
           console.log('sum ting wong');
+          deferred.reject(err);
         } else {
           console.log('data saved successfully!');
+          deferred.resolve(event);
         }
       });
     }
   });
+  
+  return deferred.promise;
 }
 
 /**
@@ -77,15 +99,3 @@ function parseEventRes (res) {
   
   return res.events[0];
 };
-
-/**
- * Add current iso datetime prop to obj
- * @param {object} obj
- * @return {object}
- */
-function addDate (obj) {
-  const date = (new Date()).toISOString();
-  return Object.assign({}, obj, {
-    date: date
-  });
-}
